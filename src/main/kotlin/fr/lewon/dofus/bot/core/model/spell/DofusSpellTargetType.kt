dca5427f1fbe4c5a0ca90ddce4be4f7a1e067dc9
viclew <viclew@gmail.com>
2022-11-01 21:10:02 +0100

    Target type mask as list

--- src/main/kotlin/fr/lewon/dofus/bot/core/model/spell/DofusSpellTargetType.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/core/model/spell/DofusSpellTargetType.kt
@@ -4,38 +4,78 @@ import fr.lewon.dofus.bot.core.fighter.IDofusFighter
 import fr.lewon.dofus.bot.core.fighter.PlayerType
 
 enum class DofusSpellTargetType(
-    private val key: Char,
+    private val keys: List<Char>,
     private val isTargetValidChecker: (Int?, IDofusFighter, IDofusFighter) -> Boolean
 ) {
-    ALLIES('a', { _, caster, target -> caster.getFighterTeamId() == target.getFighterTeamId() }),
-    ENEMIES('A', { _, caster, target -> caster.getFighterTeamId() != target.getFighterTeamId() }),
-    CASTER('C', { _, caster, target -> caster.getFighterTeamId() == target.getFighterTeamId() }),
-    SPECIFIC_ALLIED_SUMMON('F', { id, _, target -> target.getPlayerType() != PlayerType.HUMAN && target.getBreed() == id }),
-    SWITCHABLE_ENEMIES('i', { _, caster, target ->
+    ALLIES(listOf('a'), { _, caster, target ->
+        caster.getFighterTeamId() == target.getFighterTeamId()
+    }),
+    ENEMIES(listOf('A'), { _, caster, target ->
+        caster.getFighterTeamId() != target.getFighterTeamId()
+    }),
+    CASTER(listOf('c', 'C'), { _, caster, target ->
+        caster.getFighterId() == target.getFighterId()
+    }),
+    SPECIFIC_ALLIED_SUMMON(listOf('F'), { id, _, target ->
+        target.getPlayerType() != PlayerType.HUMAN && target.getBreed() == id
+    }),
+    SWITCHABLE_ENEMIES(listOf('i'), { _, caster, target ->
         caster.getFighterTeamId() != target.getFighterTeamId()
                 && target.getPlayerType() != PlayerType.SIDEKICK
                 && target.isSummon()
                 && !target.isStaticElement()
     }),
-    PLAYER_SUMMON('P', { _, caster, target ->
+    PLAYER_SUMMON(listOf('P', 'p'), { _, caster, target ->
         target.getFighterId() == caster.getFighterId()
                 || target.isSummon() && target.getSummonerId() == caster.getFighterId()
-                || !target.isSummon() && caster.getSummonerId() == target.getSummonerId()
-                || !caster.isSummon() && caster.getSummonerId() == target.getFighterId()
+                || target.isSummon() && caster.getSummonerId() == target.getSummonerId()
+                || caster.isSummon() && caster.getSummonerId() == target.getFighterId()
     }),
-    WITHOUT_STATE('e', { id, _, target -> id != null && !target.hasState(id) }),
-    WITH_STATE('E', { id, _, target -> id != null && target.hasState(id) }),
-    ENEMY_SUMMON('J', { _, caster, target -> target.getPlayerType() != PlayerType.SIDEKICK && target.getFighterTeamId() != caster.getFighterTeamId() && target.isSummon()}),
-    ALLIED_SUMMON('j', {_, caster, target -> target.getFighterTeamId() == caster.getFighterTeamId() && target.getPlayerType() != PlayerType.SIDEKICK && target.isSummon()}),
-    ENEMY_PLAYER('L',{ _, caster, target -> target.getFighterTeamId() != caster.getFighterTeamId() && (target.getPlayerType() == PlayerType.HUMAN && !target.isSummon() || target.getPlayerType() == PlayerType.SIDEKICK) }),
-    ALLIED_PLAYER('l',{_, caster, target -> target.getFighterTeamId() == caster.getFighterTeamId() && (target.getPlayerType() == PlayerType.HUMAN && !target.isSummon() || target.getPlayerType() == PlayerType.SIDEKICK)}),
-    NON_HUMAN_NON_SUMMON_ALLY('m',{_, caster, target -> target.getFighterTeamId() == caster.getFighterTeamId() && target.getPlayerType() != PlayerType.HUMAN && !target.isSummon() && !target.isStaticElement()}),
-    NON_HUMAN_NON_SUMMON_ENEMY('M',{_, caster, target -> target.getFighterTeamId() != caster.getFighterTeamId() && target.getPlayerType() != PlayerType.HUMAN && !target.isSummon() && !target.isStaticElement()})
+    WITHOUT_STATE(listOf('e'), { id, _, target ->
+        id != null && !target.hasState(id)
+    }),
+    WITH_STATE(listOf('E'), { id, _, target ->
+        id != null && target.hasState(id)
+    }),
+    ENEMY_SUMMON(listOf('J'), { _, caster, target ->
+        target.getPlayerType() != PlayerType.SIDEKICK
+                && target.getFighterTeamId() != caster.getFighterTeamId()
+                && target.isSummon()
+    }),
+    ALLIED_SUMMON(listOf('j'), { _, caster, target ->
+        target.getFighterTeamId() == caster.getFighterTeamId()
+                && target.getPlayerType() != PlayerType.SIDEKICK
+                && target.isSummon()
+    }),
+    ENEMY_PLAYER(listOf('L'), { _, caster, target ->
+        target.getFighterTeamId() != caster.getFighterTeamId()
+                && (target.getPlayerType() == PlayerType.HUMAN
+                && !target.isSummon()
+                || target.getPlayerType() == PlayerType.SIDEKICK)
+    }),
+    ALLIED_PLAYER(listOf('l'), { _, caster, target ->
+        target.getFighterTeamId() == caster.getFighterTeamId()
+                && (target.getPlayerType() == PlayerType.HUMAN
+                && !target.isSummon()
+                || target.getPlayerType() == PlayerType.SIDEKICK)
+    }),
+    NON_HUMAN_NON_SUMMON_ALLY(listOf('m'), { _, caster, target ->
+        target.getFighterTeamId() == caster.getFighterTeamId()
+                && target.getPlayerType() != PlayerType.HUMAN
+                && !target.isSummon()
+                && !target.isStaticElement()
+    }),
+    NON_HUMAN_NON_SUMMON_ENEMY(listOf('M'), { _, caster, target ->
+        target.getFighterTeamId() != caster.getFighterTeamId()
+                && target.getPlayerType() != PlayerType.HUMAN
+                && !target.isSummon()
+                && !target.isStaticElement()
+    })
     ;
 
     companion object {
         fun fromString(targetChar: Char): DofusSpellTargetType? {
-            return values().firstOrNull { it.key == targetChar }
+            return values().firstOrNull { it.keys.contains(targetChar) }
         }
     }
 
