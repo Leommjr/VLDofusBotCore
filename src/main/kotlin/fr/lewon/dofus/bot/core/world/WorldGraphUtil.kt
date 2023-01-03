7e684a0d29a8a5d96a6a7fcc01e66fac7fad8dea
viclew <viclew@gmail.com>
2023-01-03 20:11:11 +0100

    Small signature change

--- src/main/kotlin/fr/lewon/dofus/bot/core/world/WorldGraphUtil.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/core/world/WorldGraphUtil.kt
@@ -69,19 +69,10 @@ object WorldGraphUtil {
         characterInfo: DofusCharacterBasicInfo
     ): List<Transition>? {
         val fromVertex = vertices[fromMap.id]?.get(fromZone) ?: return null
-        return doGetPath(listOf(fromVertex), toMaps, characterInfo)
+        return getPath(listOf(fromVertex), toMaps, characterInfo)
     }
 
     fun getPath(
-        fromMaps: List<DofusMap>,
-        toMaps: List<DofusMap>,
-        characterInfo: DofusCharacterBasicInfo
-    ): List<Transition>? {
-        val fromVertices = fromMaps.mapNotNull { getVertex(it.id, 1) }
-        return doGetPath(fromVertices, toMaps, characterInfo)
-    }
-
-    private fun doGetPath(
         fromVertices: List<Vertex>,
         toMaps: List<DofusMap>,
         characterInfo: DofusCharacterBasicInfo
